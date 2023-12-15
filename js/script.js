e20de44a627151a4c980df0ffdd11d3e497ad924
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2023-12-15 15:38:13 +0100

    add.  get Elment & user variables

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//Js - DOM response test (lineup 1.)
+console.log('JS OK');
+
+//(lineup 2.)
+document.getElementById('printer');
+console.log(printer);
+
+//(lineup 3.)
+let firstName = prompt('Inserisci il tuo nome:', 'Lorenzo');
+console.log(firstName)
+
+let lastName = prompt('Inserisci il tuo Cognome:', 'Chierisini');
+console.log(lastName)
+
+let favColor = prompt('Inserisci il tuo Colore preferito:', 'Nero');
+console.log(favColor)
